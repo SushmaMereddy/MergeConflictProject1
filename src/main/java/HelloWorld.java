0a7ce9afc6550acfd491d66c6d06ac667ecf7dbf
SushmaMereddy <sushmamr231@gmail.com>
2024-11-25 23:26:58 -0600

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    public static void printSomething(){
+        System.out.println("Print something");
+    }
+}

42a570f95ea4387909756aef858512d8395cf35f
SushmaMereddy <sushmamr231@gmail.com>
2024-11-25 23:46:29 -0600

    changes from feature branch

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Welcome world");
     }
     public static void printSomething(){
         System.out.println("Print something");
     }
     public static void doSomething(){
-        System.out.println("Do something");
+        System.out.println("Please Do something");
     }
 }

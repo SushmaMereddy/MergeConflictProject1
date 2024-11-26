6b07d399dcf487f1bde5371d4f5bd79c9b5ccf1a
SushmaMereddy <sushmamr231@gmail.com>
2024-11-25 23:32:08 -0600

    doSomthing method is added to this commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -5,4 +5,7 @@ public class HelloWorld {
     public static void printSomething(){
         System.out.println("Print something");
     }
+    public static void doSomething(){
+        System.out.println("Do something");
+    }
 }

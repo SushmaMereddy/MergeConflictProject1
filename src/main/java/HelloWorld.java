0820d291a9b4d0b05667ffc57108ad4e7ff43dd4
SushmaMereddy <sushmamr231@gmail.com>
2024-11-25 23:42:12 -0600

    modified helloworld method and printSomething method

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world !!");
     }
     public static void printSomething(){
-        System.out.println("Print something");
+        System.out.println("Please Print something");
     }
     public static void doSomething(){
         System.out.println("Do something");
